0656c6e066a3012f8db661a69f14914e655d38f0
John Carvs <joaovitor772@hotmail.com>
2023-10-23 00:36:35 -0300

    layout quase pronto (falta a parte da logica)

--- tipCalculator/src/App.jsx
+++ tipCalculator/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div>
       
       <div className='main'>
-        
+
         <div className='inputElements'>
           <label>Bill</label><p/>
           <input></input><p/>
@@ -14,22 +14,20 @@ function App() {
           <label>Select Tip %</label><p/>
           <table id='tipOptions'>
             <tr>
-              <td id='tipOption'>5%</td>
-              <td id='tipOption'>10%</td>
-              <td id='tipOption'>15%</td>
+              <td><button id='tipOption'>5%</button></td>
+              <td><button id='tipOption'>10%</button></td>
+              <td><button id='tipOption'>15%</button></td>
             </tr>
             <tr>
-              <td id='tipOption'>25%</td>
-              <td id='tipOption'>50%</td>
-              <td id='tipOption'> 
-                <input></input>
-              </td>
+              <td><button id='tipOption'>25%</button></td>
+              <td><button id='tipOption'>50%</button></td>
+              <td><input id='tipOption' style={{paddingRight:0}}></input></td>
             </tr>
-
           </table>
-
+          <label>Number of People</label><p/>
+          <input></input>
         </div>
-        <div className='outputElements'>kkkkkkk
+        <div className='outputElements'>output
         </div>
 
       </div>
