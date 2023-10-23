7de20e96cf41aa11b04db47402854969c263fad3
John Carvs <joaovitor772@hotmail.com>
2023-10-22 23:06:24 -0300

    uptade do progesso inicial

--- tipCalculator/src/App.jsx
+++ tipCalculator/src/App.jsx
@@ -1,7 +1,40 @@
+import './App.css';
+
 function App() {
 
   return (
-    <p>Hello World</p>
+    <div>
+      
+      <div className='main'>
+        
+        <div className='inputElements'>
+          <label>Bill</label><p/>
+          <input></input><p/>
+
+          <label>Select Tip %</label><p/>
+          <table id='tipOptions'>
+            <tr>
+              <td id='tipOption'>5%</td>
+              <td id='tipOption'>10%</td>
+              <td id='tipOption'>15%</td>
+            </tr>
+            <tr>
+              <td id='tipOption'>25%</td>
+              <td id='tipOption'>50%</td>
+              <td id='tipOption'> 
+                <input></input>
+              </td>
+            </tr>
+
+          </table>
+
+        </div>
+        <div className='outputElements'>kkkkkkk
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
